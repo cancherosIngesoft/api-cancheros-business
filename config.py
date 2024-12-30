b04dbaeeef1ec149d46dadd64656e132e0650ae2
jcortesj <juancortes@habi.co>
2024-12-30 18:18:02 -0500

    added code-quality

--- config.py
+++ config.py
@@ -1,16 +1,13 @@
-import os
-
 class Config:
-    pass
-    """
-    SECRET_KEY = os.getenv('SECRET_KEY', 'tu_secreto')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///cancheros.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    """
+    """Base configuration class."""
 
-class TestConfig(Config):
-    pass
-    """
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-    """
+    DEBUG = False
+    TESTING = False
+    # ... otros atributos de configuración
+
+
+class DevelopmentConfig(Config):
+    """Development configuration class."""
+
+    DEBUG = True
+    # ... otros atributos específicos de desarrollo
