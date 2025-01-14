d2bb3f0e220384adb9d28f8509ca62f4cbbac464
SimonRamos88 <joramos@unal.edu.co>
2025-01-14 11:53:30 -0500

    commit despues de ahcer pull

--- app/models/Horario_establecimiento.py
+++ app/models/Horario_establecimiento.py
@@ -4,5 +4,5 @@ class Horario_establecimiento(db.Model):
     id_establecimiento = db.Column('id_establecimiento', db.Integer, db.ForeignKey('establecimiento.id_establecimiento'), primary_key=True)
     id_horario = db.Column(db.Integer, db.ForeignKey('horario.id_horario'), primary_key=True)
 
-    establecimiento= db.relationship('Establecimiento', back_populates='establecimientos')
-    horario = db.relationship('Horario', back_populates='canchas')
+    # establecimiento= db.relationship('Establecimiento', back_populates='establecimientos')
+    # horario = db.relationship('Horario', back_populates='canchas')
